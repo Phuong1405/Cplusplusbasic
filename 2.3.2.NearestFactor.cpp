7cde2e5b278cedd867d666293492a706f934a7dd
Phuong1405 <73667034+Phuong1405@users.noreply.github.com>
2023-09-27 22:01:09 +0300

    Finding the nearest factors for an integer number

---
+++ 2.3.2.NearestFactor.cpp
@@ -0,0 +1,26 @@
+#include <iostream> //input and output 
+using namespace std;
+int main() {
+/*    int positive_number;
+    cout << "Enter a positive number: " ;
+    cin >> positive_number;
+    if(positive_number <=0){
+        cout << "Only positive numbers accepted";
+    }
+    else{
+        while
+    }
+}*/
+    int positive_number;
+    int i_round;
+    cout << "Enter a positive number: " ;
+    cin >> positive_number;
+    
+    for(int i = 1; i<=positive_number; i++){
+        
+
+        
+        //cout <<  << endl;
+    }
+
+}
